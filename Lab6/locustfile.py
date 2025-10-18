50a114e24857885999a20687f681b7b8a7a8943a
Дорогин Никита Сергеевич <nikitadorogin@yandex.ru>
2025-10-18 23:01:08 +0700

    Add passing Shikimori defence

--- Lab6/locustfile.py
+++ Lab6/locustfile.py
@@ -1,4 +1,4 @@
-from locust import HttpUser, task
+from locust import HttpUser, task, between
 from requests.auth import HTTPBasicAuth
 
 class openBmcTester(HttpUser):
@@ -23,7 +23,7 @@ class openBmcTester(HttpUser):
             if(not has_Power): print("NO POWER")
             return response != None and has_Status and has_Power
         except Exception:
-            print("API DOESN'T ANSWER")
+            print("OpenBMC API DOESN'T ANSWER")
             return False
  
     @task(2)
@@ -40,25 +40,38 @@ class openBmcTester(HttpUser):
 
             return (power != None)
         except Exception:
-            print("API DOESN'T ANSWER")
+            print("OpenBMC API DOESN'T ANSWER")
             return False
         
 
 
 class shikimoriTester(HttpUser):
-    host = 'https://shikimori.one/NekitD'
-    auth = HTTPBasicAuth('NekitD', 'rfgbnjirf88')
+    host = 'https://shikimori.one'
     
-    def on_start(self):
-        pass
 
+    #--------------------------------------------------------------------------------
+    # Имитация браузера и поведения человека в нём, чтобы Шикимори не думал, что мы бот
+    wait_time = between(1, 3)
+    def on_start(self):
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+            "Accept-Language": "ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "none",
+        }
+    #-----------------------------------------------------------------------------------------
     def on_stop(self):
         pass
     
     @task(1)
     def anime_list(self):
         try:
-            response = self.client.get('/list/anime?order=name', auth=self.auth, verify= False)
+            response = self.client.get('/animes', verify= False, headers=self.headers )
             data = response.json()
             has_Name = 'name' in data
             has_Rate = 'rate_score' in data
@@ -70,13 +83,13 @@ class shikimoriTester(HttpUser):
             if(not has_Kind): print("LIST: NO KIND")
             return response != None and has_Name and has_Kind and has_Eps and has_Rate
         except Exception:
-            print("API DOESN'T ANSWER")
+            print("Shikimori API DOESN'T ANSWER")
             return False
     
     @task(2)
     def get_first(self):
         try:
-            response = self.client.get('/list/anime?order=name', auth=self.auth, verify= False)
+            response = self.client.get('/animes', verify= False, headers=self.headers)
             data = response.json()
             first = data[0]
             has_Name = 'name' in first
@@ -88,10 +101,10 @@ class shikimoriTester(HttpUser):
             if(not has_Eps): print("FIRST: NO EPISODES")
             if(not has_Kind): print("FIRST: NO KIND")
 
-            if(first['name'] == "Аля иногда кокетничает со мной по-русски"): return True
+            if(first['name'] == "Провожающая в последний путь Фрирен"): return True
             else: return False
-            
+
         except Exception:
-            print("API DOESN'T ANSWER")
+            print("Shikimori API DOESN'T ANSWER")
             return False
 
